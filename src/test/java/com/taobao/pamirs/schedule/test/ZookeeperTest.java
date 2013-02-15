948aeda73a2bdf08bccaae90f1b05f66eb53dcda
xuannan <xuannan@taobao.com>
2013-02-15 07:38:38 +0000

    修改Watcher实现方式

--- src/test/java/com/taobao/pamirs/schedule/test/ZookeeperTest.java
+++ src/test/java/com/taobao/pamirs/schedule/test/ZookeeperTest.java
@@ -20,7 +20,7 @@ public class ZookeeperTest {
 @Test
 	 public void testCloseStatus() throws Exception{
 		 ZooKeeper zk = new ZooKeeper("localhost:2181", 3000,
-				 new  ScheduleWatcher());
+				 new  ScheduleWatcher(null));
 		 int i = 1;
 		 while(true){
 			 try{
@@ -36,7 +36,7 @@ public class ZookeeperTest {
  @Test
  public void testPrint() throws Exception{
 	 ZooKeeper zk = new ZooKeeper("localhost:2181", 3000,
-			 new  ScheduleWatcher());
+			 new  ScheduleWatcher(null));
 	 StringWriter writer = new StringWriter();
 	 ZKTools.printTree(zk, "/", writer,"\n");
 	 System.out.println(writer.getBuffer().toString());
@@ -44,7 +44,7 @@ public class ZookeeperTest {
  @Test
  public void deletePath() throws Exception{
 	 ZooKeeper zk = new ZooKeeper("localhost:2181", 3000,
-			 new  ScheduleWatcher());
+			 new  ScheduleWatcher(null));
 	 zk.addAuthInfo("digest","ScheduleAdmin:password".getBytes());
 
 	 ZKTools.deleteTree(zk,"/taobao-pamirs-schedule");
@@ -55,7 +55,7 @@ public class ZookeeperTest {
 
 	@Test
 	public void testACL() throws Exception {
-		ZooKeeper zk = new ZooKeeper("localhost:2181", 3000,new  ScheduleWatcher());
+		ZooKeeper zk = new ZooKeeper("localhost:2181", 3000,new  ScheduleWatcher(null));
 		List<ACL> acls = new ArrayList<ACL>();
 		zk.addAuthInfo("digest","TestUser:password".getBytes());
 		acls.add(new ACL(ZooDefs.Perms.ALL,new Id("digest",DigestAuthenticationProvider.generateDigest("TestUser:password"))));
