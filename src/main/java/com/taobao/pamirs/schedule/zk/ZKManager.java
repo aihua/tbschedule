7b26a3d267f12209405c852199d37589d471f4a2
jishao <jishao@taobao.com>
2014-03-20 03:46:01 +0000

    重要：修复schedule重新连接zookeeper会追加acl信息问题

--- src/main/java/com/taobao/pamirs/schedule/zk/ZKManager.java
+++ src/main/java/com/taobao/pamirs/schedule/zk/ZKManager.java
@@ -36,7 +36,7 @@ public class ZKManager{
 	}
 	
 	/**
-	 * ï¿½ï¿½ï¿½ï¿½zookeeper
+	 * ÖØÁ¬zookeeper
 	 * @throws Exception
 	 */
 	public synchronized void  reConnection() throws Exception{
@@ -66,7 +66,7 @@ public class ZKManager{
 				+ ":"+ this.properties.getProperty(keys.password.toString());
 		this.isCheckParentPath = Boolean.parseBoolean(this.properties.getProperty(keys.isCheckParentPath.toString(),"true"));
 		zk.addAuthInfo("digest", authString.getBytes());
-        acl.clear();
+		acl.clear();
 		acl.add(new ACL(ZooDefs.Perms.ALL, new Id("digest",
 				DigestAuthenticationProvider.generateDigest(authString))));
 		acl.add(new ACL(ZooDefs.Perms.READ, Ids.ANYONE_ID_UNSAFE));
@@ -74,20 +74,20 @@ public class ZKManager{
 	
 	private void sessionEvent(CountDownLatch connectionLatch, WatchedEvent event) {
 		if (event.getState() == KeeperState.SyncConnected) {
-			log.info("ï¿½Õµï¿½ZKï¿½ï¿½ï¿½Ó³É¹ï¿½ï¿½Â¼ï¿½ï¿½ï¿½");
+			log.info("ÊÕµ½ZKÁ¬½Ó³É¹¦ÊÂ¼þ£¡");
 			connectionLatch.countDown();
 		} else if (event.getState() == KeeperState.Expired) {
-			log.error("ï¿½á»°ï¿½ï¿½Ê±ï¿½ï¿½ï¿½È´ï¿½ï¿½ï¿½ï¿½Â½ï¿½ï¿½ï¿½ZKï¿½ï¿½ï¿½ï¿½...");
+			log.error("»á»°³¬Ê±£¬µÈ´ýÖØÐÂ½¨Á¢ZKÁ¬½Ó...");
 			try {
 				reConnection();
 			} catch (Exception e) {
 				log.error(e.getMessage(),e);
 			}
-		} // Disconnectedï¿½ï¿½Zookeeperï¿½ï¿½ï¿½Ô¶ï¿½ï¿½ï¿½ï¿½ï¿½Disconnected×´Ì¬ï¿½ï¿½ï¿½ï¿½
+		} // Disconnected£ºZookeeper»á×Ô¶¯´¦ÀíDisconnected×´Ì¬ÖØÁ¬
 	}
 	
 	public void close() throws InterruptedException {
-		log.info("ï¿½Ø±ï¿½zookeeperï¿½ï¿½ï¿½ï¿½");
+		log.info("¹Ø±ÕzookeeperÁ¬½Ó");
 		this.zk.close();
 	}
 	public static Properties createProperties(){
@@ -111,16 +111,16 @@ public class ZKManager{
 		return zk != null && zk.getState() == States.CONNECTED;
 	}
 	public void initial() throws Exception {
-		//ï¿½ï¿½zk×´Ì¬ï¿½ï¿½ï¿½ï¿½ï¿½Üµï¿½ï¿½ï¿½
+		//µ±zk×´Ì¬Õý³£ºó²ÅÄÜµ÷ÓÃ
 		if(zk.exists(this.getRootPath(), false) == null){
 			ZKTools.createPath(zk, this.getRootPath(), CreateMode.PERSISTENT, acl);
 			if(isCheckParentPath == true){
 			  checkParent(zk,this.getRootPath());
 			}
-			//ï¿½ï¿½ï¿½Ã°æ±¾ï¿½ï¿½Ï¢
+			//ÉèÖÃ°æ±¾ÐÅÏ¢
 			zk.setData(this.getRootPath(),Version.getVersion().getBytes(),-1);
 		}else{
-			//ï¿½ï¿½Ð£ï¿½é¸¸ï¿½×½Úµã£¬ï¿½ï¿½ï¿½ï¿½ï¿½Ç·ï¿½ï¿½Ñ¾ï¿½ï¿½ï¿½scheduleï¿½ï¿½Ä¿Â¼
+			//ÏÈÐ£Ñé¸¸Ç×½Úµã£¬±¾ÉíÊÇ·ñÒÑ¾­ÊÇscheduleµÄÄ¿Â¼
 			if(isCheckParentPath == true){
 			   checkParent(zk,this.getRootPath());
 			}
@@ -130,9 +130,9 @@ public class ZKManager{
 			}else{
 				String dataVersion = new String(value);
 				if(Version.isCompatible(dataVersion)==false){
-					throw new Exception("TBScheduleï¿½ï¿½ï¿½ï¿½æ±¾ "+ Version.getVersion() +" ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Zookeeperï¿½Ðµï¿½ï¿½ï¿½Ý°æ±¾ " + dataVersion );
+					throw new Exception("TBSchedule³ÌÐò°æ±¾ "+ Version.getVersion() +" ²»¼æÈÝZookeeperÖÐµÄÊý¾Ý°æ±¾ " + dataVersion );
 				}
-				log.info("ï¿½ï¿½Ç°ï¿½Ä³ï¿½ï¿½ï¿½æ±¾:" + Version.getVersion() + " ï¿½ï¿½Ý°æ±¾: " + dataVersion);
+				log.info("µ±Ç°µÄ³ÌÐò°æ±¾:" + Version.getVersion() + " Êý¾Ý°æ±¾: " + dataVersion);
 			}
 		}
 	}
